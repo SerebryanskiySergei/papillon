570994111e707f59097a888e5a69ded796c2ddf1
SerebryanskiySergei <serebryanskiysergei@gmail.com>
2019-11-14 23:37:11 +0300

    Update our services section

--- resources/views/home/our-services.blade.php
+++ resources/views/home/our-services.blade.php
@@ -9,33 +9,33 @@
                             <div class="icon-bx-wraper center">
                                 <div class="icon-xl text-primary m-b10"> <a href="#" class="icon-cell text-primary"><i class="flaticon-relax-1"></i></a> </div>
                                 <div class="icon-content">
-                                    <h3 class="dez-tilte text-uppercase font-weight-600">Relish In Rest</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod..</p>
+                                    <h3 class="dez-tilte text-uppercase font-weight-600">Быстро</h3>
+                                    <p>Только в нашем салоне процесс депиялции пройдет максимально быстро. Мы используем тоьлко щадящие и натуральные средства.</p>
                                 </div>
                             </div>
-                        </div>							
+                        </div>
                     </div>
                     <div class="item">
                         <div class="spa-service-box">
                             <div class="icon-bx-wraper center">
                                 <div class="icon-xl text-primary m-b10"> <a href="#" class="icon-cell text-primary"><i class="flaticon-female-hairs"></i></a> </div>
                                 <div class="icon-content">
-                                    <h3 class="dez-tilte text-uppercase font-weight-600">Trendy Hair Style</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod..</p>
+                                    <h3 class="dez-tilte text-uppercase font-weight-600">Качественно</h3>
+                                    <p>Мы используем только сертифицированную продукцию исключительно высшего качества, с который пир желании ты можешь ознакомиться и попробовать самостоятельно.</p>
                                 </div>
                             </div>
-                        </div>							
+                        </div>
                     </div>
                     <div class="item">
                         <div class="spa-service-box">
                             <div class="icon-bx-wraper center">
                                 <div class="icon-xl text-primary m-b10"> <a href="#" class="icon-cell text-primary"><i class="flaticon-massage"></i></a> </div>
                                 <div class="icon-content">
-                                    <h3 class="dez-tilte text-uppercase font-weight-600">Pebble Treatment</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod..</p>
+                                    <h3 class="dez-tilte text-uppercase font-weight-600">Комфортно</h3>
+                                    <p>Наш мастер работает в данной сфере уже более 5-ти лет и испробовал разные методики удаления волос. Это неверотяно, но у нас процедура стала практически безболезненной в связи с тем, что наш мастер владеет всеми техниками исполнения и использует индивидуальный подход к каждому, комбинирую техники и продукцию.</p>
                                 </div>
                             </div>
-                        </div>							
+                        </div>
                     </div>
                 </div>
             </div>
@@ -50,7 +50,7 @@
                     <div class="dis-tbl-cell">
                         <h2 class="text-primary spa-title-head ">Special Treatments</h2>
                         <div class="dez-separator spa bg-primary"></div>
-                        <ul class="list-icon-box m-t30"> 
+                        <ul class="list-icon-box m-t30">
                             <li><i class="flaticon-nature text-primary"></i><span>Keep your skin radiant this winter.</span></li>
                             <li><i class="flaticon-stones text-primary"></i><span>Created from natural herbs – Ginseng roots extract.</span></li>
                             <li><i class="flaticon-spa text-primary"></i><span>Provide treatments and products that are safe, sanitary, and effective.</span></li>
