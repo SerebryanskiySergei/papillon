eb9a44be22332a10f98d4f06735bafdf09165282
SerebryanskiySergei <serebryanskiysergei@gmail.com>
2019-11-28 02:12:45 +0300

    Addere reviews text and send review modal

--- resources/views/home/reviews.blade.php
+++ resources/views/home/reviews.blade.php
@@ -1,4 +1,4 @@
-<div class="section-full overlay-black-middle bg-img-fix content-inner" style="background-image:url(images/background/bg3.jpg);">
+<div class="section-full overlay-black-middle bg-img-fix content-inner" style="background-image:url(images/background/reviews.jpg);">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -11,55 +11,95 @@
                             <div class="testimonial-3 testimonial-bg">
                                 <div class="quote-left"></div>
                                 <div class="testimonial-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>
+                                    <p>Хожу в этот салон на шугаринг с сентября. Очень нравится то, как быстро проводится процедура и за разговором с мастером не замечаешь дискомфорта))</br>
+                                        Удивилась большому количеству ухаживающих средств. Видно, что мастер с удовольствием выполняет свою работу.</br>
+                                        У меня довольно чувствительная кожа, но раздражение проходит уже через пять минут!!!</br>
+                                        Спасибо мастеру Лизе)))</p>
                                 </div>
                                 <div class="testimonial-detail"> <strong class="testimonial-name">Даша</strong> <span class="testimonial-position">Студентка</span> </div>
-                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>
                             </div>
                         </div>
                         <div class="item">
                             <div class="testimonial-3 testimonial-bg">
                                 <div class="quote-left"></div>
                                 <div class="testimonial-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>
+                                    <p>В этом салоне работает супер мастер!</br>
+                                        У меня после многократных попыток найти мастера с лёгкой рукой, совсем пропало желание делать депиляцию.
+                                        Спасибо дочке, уговорила меня отказаться от бритвы и прийти к Лизе.</br>
+                                        Она в начале расспросила о том, насколько чувствительна моя кожа и описала несколько способов депиляции, предоставив мне выбор.
+                                        После нескольких сеансов у меня практически полностью прошло врастание!</br>
+                                        Очень довольна! Рекомендую всем салон Papillon!</p>
                                 </div>
                                 <div class="testimonial-detail"> <strong class="testimonial-name">Женя</strong> <span class="testimonial-position">Модель</span> </div>
-                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>
                             </div>
                         </div>
                         <div class="item">
                             <div class="testimonial-3 testimonial-bg">
                                 <div class="quote-left"></div>
                                 <div class="testimonial-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>
+                                    <p>Было очень стремно идти. Моя девушка взяла с меня слово, что я схожу на депиляцию подмышек. Поспрашивал знакомых, услышал много позитивных откликов про мастера из Papillon.</br>
+                                        В итоге ни разу не пожалел что пришел! Очень круто все сделано, сама процедура прошла достаточно легко! </br>
+                                        Очень приятный мастер, видя что я немного стесняюсь, повела себя очень профессионально! </br>
+                                        Через месяц пойду еще)
+                                    </p>
                                 </div>
                                 <div class="testimonial-detail"> <strong class="testimonial-name">Сергей</strong> <span class="testimonial-position">Программист</span> </div>
-                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>
                             </div>
                         </div>
-                        <div class="item">
-                            <div class="testimonial-3 testimonial-bg">
-                                <div class="quote-left"></div>
-                                <div class="testimonial-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>
-                                </div>
-                                <div class="testimonial-detail"> <strong class="testimonial-name">Алена</strong> <span class="testimonial-position">Старший менеджер</span> </div>
-                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <div class="testimonial-3 testimonial-bg">
-                                <div class="quote-left"></div>
-                                <div class="testimonial-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>
-                                </div>
-                                <div class="testimonial-detail"> <strong class="testimonial-name">Александра</strong> <span class="testimonial-position">Бухгалтер</span> </div>
-                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>
-                            </div>
-                        </div>
+{{--                        <div class="item">--}}
+{{--                            <div class="testimonial-3 testimonial-bg">--}}
+{{--                                <div class="quote-left"></div>--}}
+{{--                                <div class="testimonial-text">--}}
+{{--                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy [...]</p>--}}
+{{--                                </div>--}}
+{{--                                <div class="testimonial-detail"> <strong class="testimonial-name">Александра</strong> <span class="testimonial-position">Бухгалтер</span> </div>--}}
+{{--                                <div class="testimonial-pic radius shadow"><img src="images/testimonials/pic1.jpg" width="100" height="100" alt=""></div>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
                     </div>
+                </div>
+                <div class="sort-title text-white clearfix text-center">
+                    <a data-toggle="modal" data-target="#myModal" class="site-button radius-xl m-lr5">Оставить отзыв</a>
                 </div>
             </div>
         </div>
     </div>
+</div>
+<!-- Modal -->
+<div class="modal fade contact-form black" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <form method="post" class="p-b20" action="reviews">
+                    <div class="row">
+                        <button name="Resat" type="reset" value="Reset" data-dismiss="modal" class="close-btn">&times; </button>
+                        <div class="col-md-12 text-center text-white">
+                            <h2 class="text-white"></h2>
+                            <p>Дайте нам знать, что вы думаете о нашем салоне!</p>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="m-b15">
+                                <input id="review_author" placeholder="Ваше имя" name="author" type="text" required class="form-control radius-xl">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="m-b15">
+                                <input id="review_author_job" placeholder="Ваша профессия" name="author_job" type="text" class="form-control radius-xl">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="m-b15">
+                                <textarea class="form-control radius-xl" placeholder="Что вы думаете?" id="review_text" name="text" rows="8" required></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <button name="submit" type="submit" value="Submit" class="site-button radius-xl outline white btn-block">Оставить отзыв!</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
 </div>
